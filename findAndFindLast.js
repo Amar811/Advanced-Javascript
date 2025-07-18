e834a677205b0b2b7a4f557698b8a290849687b4
Amar Ram <ramamar151@gmail.com>
2025-07-18 15:44:45 +0530

    Find and findindex, and findlast and findlastindex

--- findAndFindLast.js
+++ findAndFindLast.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const users = [
+  { id: 1, name: "apple", price: 20 },
+  { id: 2, name: "mango", price: 30 },
+  { id: 3, name: "pineapple", price: 50 },
+  { id: 6, name: "apple", price: 90 },
+  { id: 7, name: "mango", price: 110 },
+  { id: 10, name: "pineapple", price: 150 },
+];
+
+const result = users.findLastIndex((item) => item.price > 50);
+console.log(result);
